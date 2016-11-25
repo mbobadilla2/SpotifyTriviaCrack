40a7ab3f18d4c5ca1e48c8ab3cdbb2db7b012cd5
Miguel Bobadilla <mbobadilla@outlook.com>
2016-11-24 20:47:35 -0600

    Modal window
    + Added a modal window that appears when the user clicks on a playlist.
    This modal window will be the area where the user will play the round.
    
    + The number of songs selected from the ajax request was reduced to
    five.
    
    + Minor fixes.

--- public/js/credentials.js
+++
@@ -1,5 +0,0 @@
-var client_id = 'Your_client_ID'; // Your client id
-var client_secret = 'Your_secret'; // Your secret
-var redirect_uri = 'Your_redirect_url'; // Your redirect uri (for example, http://localhost:8888/)
-
-var oauthtoken = "Bearer Your_oauthtoken";
--- public/js/main.js
+++ public/js/main.js
@@ -1,32 +1,56 @@
+var songsLimit = 5;
+var optionsLimit = 4;
+var selectedSongs = new Array(songsLimit);
+var selectedOptions = new Array(optionsLimit);
+
 $(document).ready(function(){
-    var resultTemp = $("#result").html();
+    var resultState = $("#result").html();
 
     $("a").on("click", function(e){
         e.preventDefault();
     });
 
     $("#exitosMX").on("click", function(){
-        var exitosMexico = {
+        var playlist = {
             name:"Éxitos de México",
             url: "https://api.spotify.com/v1/users/spotifyenespa%C3%B1ol/playlists/3lCaS7QcP5GzAP70rd9bpV/tracks"
         }
 
-        getPlaylist(exitosMexico, resultTemp);
+        getPlaylist(playlist, resultState);
     });
 
     $("#rockEspanol").on("click", function(){
-        var rockEspanol = {
+        var playlist = {
             name:"Rock en español",
             url: "https://api.spotify.com/v1/users/spotifyenespa%C3%B1ol/playlists/4YoI4gyyoBCIexSdjqJMfl/tracks"
         }
 
-        getPlaylist(rockEspanol, resultTemp);
+        getPlaylist(playlist, resultState);
+    });
+
+   $("#metallica").on("click", function(){
+        var playlist = {
+            name:"Metallica",
+            url: "https://api.spotify.com/v1/users/metallicaofficial/playlists/1cJ6lPBYj2fscs0kqBHsVV/tracks"
+
+        }
+        
+        getPlaylist(playlist, resultState);
+    });
+
+    $("#theDoors").on("click", function(){
+        var playlist = {
+            name: "The Doors",
+            url: "https://api.spotify.com/v1/users/1229306343/playlists/71yTvo2DPAfLW2OSbhtU0m/tracks"
+        }
+
+        getPlaylist(playlist, resultState);
     });
 
 
 });
 
-function getPlaylist(playlist, temp){
+function getPlaylist(playlist, resultState){
     $.ajax({
             url: playlist.url,
             headers: {
@@ -36,23 +60,61 @@ function getPlaylist(playlist, temp){
             accepts: "application/json",
             type: "GET",
             success: function (data) {
-                $("#result").html(temp);
+                $("#result").html(resultState);
                 console.log(data);
-                for (var i in data.items) {
-                    $("#result").append('<a class="list-group-item song" href="'+ data.items[i].track.preview_url + '">' + data.items[i].track.name + '</a>');
+
+                selectedSongs = new Array(songsLimit);
+
+                console.log("Tamaño de la lista: " + data.items.length);
+                for(var i = 0; i < songsLimit; i++){
+                    var oneSong = ranNum(data.items.length - 1, data);
+
+                    console.log("******* Selected song: " + data.items[oneSong].track.name + " | url: " + data.items[oneSong].track.preview_url); 
+                    if(data.items[oneSong].track.preview_url == null){
+                        //$("#result").append('<a style="background:steelblue; color:white" class="list-group-item song" href="'+ data.items[oneSong].track.preview_url + '">' + data.items[oneSong].track.name + ' (no url)</a>');                        
+                        console.log("**************REINICIO :v");
+                        getPlaylist(playlist, resultState);
+                    }else{
+                        $("#result").append('<a class="list-group-item song" href="' +
+                            data.items[oneSong].track.preview_url +'">' + data.items[oneSong].track.name + '</a>');
+                    }
                 }
 
                 $(".song").on("click", playSong);
+                $("#modal-playlist-title").text(playlist.name);
+                $("#myModal").modal('show');
             },
             error: function(data){
-
+                alert("There was a problem getting the playlist: " + data.status + "("+data.statusText+")" + 
+                    "\n\nError 401: The oauth token expired");
+                console.log(data);
             }
     });
 }
 
 function playSong(e){
     e.preventDefault();
-
     var preview_url = $(this).attr("href");
     $("#player").attr("src", preview_url);
+
+}
+
+function ranNum(limit, data){
+    var randomNumber = parseInt(Math.random() * limit) + 1;
+    console.log("***** Número aleatorio: " + randomNumber);
+
+    for(var i in selectedSongs){
+        if(randomNumber == selectedSongs[i] || 
+            data.items[randomNumber].track.preview_url == null ||
+                typeof data.items[randomNumber].track.preview_url == null){
+            console.log("Número repetido :'v    : " + "random: " + randomNumber + " | i : " + selectedSongs[i]);
+            console.log("O url nula :'v  : " + data.items[randomNumber].track.preview_url);
+            //break;
+            return ranNum(limit, data);
+        }
+
+    }
+
+    selectedSongs.push(randomNumber);
+    return randomNumber;
 }
