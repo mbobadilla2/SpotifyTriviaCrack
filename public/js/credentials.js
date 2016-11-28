50d251505cc54df103918493899c2c7ebe5327eb
Miguel Bobadilla <mbobadilla@outlook.com>
2016-11-28 13:32:33 -0600

    Credentials.js file

---
+++ public/js/credentials.js
@@ -0,0 +1,5 @@
+var client_id = ''; // Your client id
+var client_secret = ''; // Your secret
+var redirect_uri = ''; // Your redirect uri (http://localhost:8888/callback)
+
+var oauthtoken = "Bearer ";
